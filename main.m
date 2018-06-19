11675beabdfdf8cc3f899ccbc29833fdf23c394b
Soichi Hayashi <hayashis@iu.edu>
2018-06-19 18:44:14 -0400

    updated paths

--- main.m
+++ main.m
@@ -1,19 +1,12 @@
 function [] = main()
 
-if isempty(getenv('SERVICE_DIR'))
-    disp('setting SERVICE_DIR to pwd')
-    setenv('SERVICE_DIR', pwd)
-end
+if ~isdeployed
+    disp('loading paths (HPC)')
+    addpath(genpath('/N/u/brlife/git/vistasoft'))
+    addpath(genpath('/N/u/brlife/git/jsonlab'))
 
-switch getenv('ENV')
-    case 'IUHPC'
-        disp('loading paths (HPC)')
-        addpath(genpath('/N/u/brlife/git/vistasoft'))
-        addpath(genpath('/N/u/brlife/git/jsonlab'))
-    case 'VM'
-        disp('loading paths (VM)')
-        addpath(genpath('/usr/local/jsonlab'))
-        addpath(genpath('/usr/local/vistasoft'))
+    %addpath(genpath('/usr/local/jsonlab'))
+    %addpath(genpath('/usr/local/vistasoft'))
 end
 
 % load my own config.json
